ff225f48fc26d2481b127d038c5746d3288a0471
seekingvirtue <seeking_virtue@hotmail.com>
2023-04-24 20:39:15 -0500

    Adding folder, files and file header for chapter 6 exercises and projects

---
+++ ch6/chapter6.js
@@ -0,0 +1,12 @@
+/*
+BEGIN CHAPTER 6 starting page 142
+WE WILL LOOK AT THE FOLLOWING
+
+- Basic functions
+- Function arguments
+- Return
+- Variable scope in functions
+- Recursive Functions
+- Anonymous functions
+- Function callbacks
+*/
