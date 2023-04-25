7c1f661d1af12af937f512fd6b097c3d20e8e164
Jesse DiSimone <seeking_virtue@hotmail.com>
2023-04-25 12:21:31 -0500

    adding up to 6.3 exercise

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This is a simple tracking section for progress made toward refreshing myself on the JS Language
 
 All the code exercisews and the projects are from the attached PDF book.
+
+You'll notice that it wasn't until about ch4 that I started thinking this would be a good idea to document. ch1-3 are really messy and not well organized.
+
+Maybe I'll get around to fixing them, but like a "good" programmer, I'll wait until it's a problem.
--- ch6/chapter6.js
+++ ch6/chapter6.js
@@ -10,3 +10,96 @@ WE WILL LOOK AT THE FOLLOWING
 - Anonymous functions
 - Function callbacks
 */
+
+console.log("WRITING FUNCTIONS");
+console.log(`
+        function nameOfTheFunction() {
+            //content of the function
+        }
+`);
+{
+    // // make a function that asks for your name then greets you
+    // function sayHello() {
+    //     let you = prompt("What's your name? "); // you can have vaiables in functions
+    //     console.log("Hello", you + "!");
+    // }
+    // // call the function
+    // sayHello();
+
+    // make a function that logs to console some a phrase
+    let varContainingFunction = function() { // notice that variables can have funtions inside them
+        let varInFunction = "I'm in a function!";
+        console.log("Hi there!", varInFunction);
+    }
+    // call the function
+    varContainingFunction(); //notice the parenthesis on the variable name
+}
+
+console.log("PRACTICE EXERCISE 6.1");
+{
+    // write a function that adds two numbers
+    function addTwoNumbers(first, second) { // arguements / parameters were not introduced yet
+        return Number(first) + Number(second); // this keyword was not introduced yet
+    }
+    // declare some number variables
+    let number1 = 1;
+    let number2 = 2;
+    // add them using the function, print to console
+    console.log(addTwoNumbers(number1, number2));
+}
+
+console.log("PRACTICE EXERCISE 6.2");
+// {
+//     // write a program that will randomly describe a name
+//     // make an array of description words
+//     let descriptionsArray = ["old",
+//                             "ugly",
+//                             "young",
+//                             "beautiful",
+//                             "grouchy",
+//                             "helpful"];
+//     // make a function asking for a name
+//     function askNameAndDescribe() {
+//         let name = prompt("What's your name?");
+//         // use a random number to pick a description from the list and print to console.
+//         console.log(name, "is a", descriptionsArray[Math.floor(Math.random() * 6)], "name!"); // wasn't sure this would work, but it did!
+//     }
+//     // invoke the function
+//     askNameAndDescribe();
+// }
+
+console.log("PARAMETERS AND ARGUMENTS");
+console.log(`
+        function tester(para1, para2) {
+            return para1 + " " + para2;
+        }
+        const arg1 = "argument 1";
+        const arg2 = "argument 2";
+        tester(arg1, arg2);
+`);
+{
+    // parameters are the variables listed inside the parentheses of a function
+    // x and y in the below example are parameters
+    function addTwoNumbers(x, y) {
+        console.log(x + y);
+    }
+    // arguements are the values passed to the function when invoked
+    // 1 and 2 in this case are arguements
+    addTwoNumbers(1, 2);
+
+    //other examples of arguements
+    console.log("this is an arguement");
+    let arr = [];
+    arr.push("arguement here top");    
+}
+
+console.log("PRACTICE EXERCISE 6.3");
+{
+    // create a basic addition and subtraction calculator taking a string parameter for + or -
+    // set up vars to contain number values
+    let number1 = 1
+    let number2 = 2;
+    // set up variable to hold the operator
+    let operation = "+";
+    // create a function 
+}
