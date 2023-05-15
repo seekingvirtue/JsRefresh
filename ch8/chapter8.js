3fefc1cbd037c44e3d1d9ba11663b676ee8a5a0c
Jesse DiSimone <seeking_virtue@hotmail.com>
2023-05-15 11:50:49 -0500

    completed chpt 7, setup for chtp 8

---
+++ ch8/chapter8.js
@@ -0,0 +1,11 @@
+/*
+    CHAPTER 8 BUILT-IN JAVASCRIPT METHODS STARTING ON PAGE 192
+    In this chapter we will look at the following concepts:
+    
+    - Global JavaScript Methods
+    = String methods
+    - Math Methods
+    - Date Methods
+    - Array Methods
+    - Number methods
+*/
