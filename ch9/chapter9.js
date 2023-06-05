80c79d9b4371f388e219fb5147abbbab08e13f0f
Jesse DiSimone <seeking_virtue@hotmail.com>
2023-06-05 12:39:54 -0500

    Getting through practice exercise 9.1

--- ch9/chapter9.js
+++ ch9/chapter9.js
@@ -1,1 +1,25 @@
-//
+// the DOM = Document Object Model
+// this chapter will discuss the following topics
+/*
+    -HTML Crash Course
+    -Introducing the BOM (Browser Object Model)
+    -Introducting the DOM
+    -Types of DOM elements
+    -Selecting Page Elements
+*/
+
+// the DOM grabs an HTML page and turns it into a logical tree
+// the BOM holds all the methods and properties for JavaScript to interact with the browser
+
+// the DOM contains the HTML elements on the web page. 
+
+// the BOM is sometimes called the WINDOWS BROWSER OBJECT, it is the magic element that makes it possible for JavaScript code to communicate with the browser
+// some of the most important objects of the BOM we will look are are
+/*
+    HISTORY
+    NAVIGATOR
+    LOCATION
+*/
+
+console.log("PRACTICE EXERCISE 9.1")
+console.log(window.outerHeight, window.outerWidth, window.innerHeight, window.innerWidth)
