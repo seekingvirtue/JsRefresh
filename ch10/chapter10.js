651f9a46aaa32f22df4de51e43e1c5b6b68de1ae
Jesse DiSimone <seeking_virtue@hotmail.com>
2023-06-08 19:54:46 -0500

    Starting chapter 10

---
+++ ch10/chapter10.js
@@ -0,0 +1,14 @@
+// BEGIN CHAPTER 10
+    /*
+        Along the way we will cover the following topics
+        -Basic DOM traversing
+        -Accessing elements in the DOM
+        -Element click handler
+        -THIS and the DOM
+        -Manipulating element style
+        -Changing the classes of an element
+        -Event listeners on elements
+        -Creating new event listeners
+    */
+
+// In order to interact with our web page and create a dynamic web page, we have to connect our javascript skills to the DOM
